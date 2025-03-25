fc22b332474d6a517ec5ea38bf79110ca6796b18
Yogicyo28 <11mipa1yogicahyono@gmail.com>
2025-03-25 13:16:55 +0700

    upload tugas

---
+++ PBW3/materi/script.js
@@ -0,0 +1,68 @@
+// function handleclick(){
+// alert("halaman berhasil dimuat");
+// }
+
+// variable
+// let nama;
+// nama = "igoyy";
+// console.log(nama);
+
+// let gender;
+// gender = "laki-laki";
+// console.log(gender);
+
+// let umur = "15";
+// console.log(umur);
+
+// let warna="hitam";
+// console.log(warna);
+
+// const greeting = "selamat pagi";
+// const kota = "jakarta";
+// console.log(greeting+kota);
+
+let a=5;
+let b=2;
+console.log(a+b);
+
+let faculty = [
+    'fakultas ilmu komputer',
+    'fakultas teknik',
+    'fakultas hukum',
+];
+console.log(faculty[0]);
+console.log(faculty[1]);
+console.log(faculty[2]);
+
+faculty[0] = 'fakultas ekonomi';
+console.log(faculty[0]);
+console.log(faculty[1]);
+console.log(faculty[2]);
+
+let result = 10 + 100;
+console.log(result);
+
+let result2 = 9 < 10;
+console.log(result2);
+
+
+let male= ['yogi','jaya'];
+console.log(male);
+
+let female = ['ica', 'nisa'];
+console.log(female);
+
+let students = [...male,...female];
+console.log(students);
+
+let gender ='L';
+
+if (gender === "L"){
+    console.log("aku cowo");
+} else {
+    console.log("aku cewe");
+}
+
+
+
+
---
+++ PBW3/tugaspertemuan3/Fibonacci/fibonacci.js
@@ -0,0 +1,11 @@
+const generateFibonacci = (n) => {
+    let fibo = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        fibo.push(fibo[i - 1] + fibo[i - 2]);
+    }
+
+    return fibo.slice(0, n);
+};
+
+console.log(generateFibonacci(20));
